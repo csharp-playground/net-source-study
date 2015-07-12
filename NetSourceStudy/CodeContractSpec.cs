2a5382afa704a476297f23dfaf5bee1381fd03bd
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-07-13 00:35:02 +0700

    test precondition contract

---
+++ NetSourceStudy/CodeContractSpec.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetSourceStudy
+{
+    public class CodeContractSpec
+    {
+        [Test]
+        public void ShouldRequire()
+        {
+            var x = 100;
+            Contract.Requires(x > 100);
+        }
+    }
+}
